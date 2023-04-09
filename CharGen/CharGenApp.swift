b03c39625f824b4bbe3651b970ea188bb2067d25
Alejo <a.o.alejo@gmail.com>
2023-04-09 00:15:52 -0300

    Initial Commit

---
+++ CharGen/CharGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharGenApp.swift
+//  CharGen
+//
+//  Created by AlejoVx on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CharGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
